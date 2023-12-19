2096f34252fcf37e18e131e405900b121ecd9bf0
crontreras1 <crontreras1@gmail.com>
2023-12-19 11:13:57 -0500

    class 5. using props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
 import { Inter } from 'next/font/google'
 import { RandomFox } from '@/components/RandomFox'
+import { NextPage } from 'next'
+import exp from 'constants'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+// Random number from 1 to 122
+const randomImg = (): number => Math.floor(Math.random() * 122) + 1
+
+const Home: NextPage = () =>  {
   return (
     <main>
       <h2>Hola, mundillo ;)</h2>
-      <RandomFox /> 
+      <RandomFox image={ `https://randomfox.ca/images/${randomImg()}.jpg` }/> 
     </main>
   )
 }
+
+export default Home
