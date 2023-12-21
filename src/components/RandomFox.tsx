4c699331f90b0c872f49baf48537585184e006f7
crontreras1 <crontreras1@gmail.com>
2023-12-21 12:00:35 -0500

    class 7. state, personal types

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,5 +1,7 @@
-export const RandomFox = (): JSX.Element => {
-    const randomImg = (): number => Math.floor(Math.random() * 123) + 1
-    const image: string = `https://randomfox.ca/images/${randomImg()}.jpg`
-    return <img src={ image }/>  
+type Props = {
+    image: string;
+}
+
+export const RandomFox = ({ image }: Props): JSX.Element => {
+    return <img src={ image } className="w-96 h-auto"/>  
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,30 @@
-import { Inter } from 'next/font/google'
+import { useState } from 'react'
 import { RandomFox } from '@/components/RandomFox'
 import { NextPage } from 'next'
-import exp from 'constants'
-
-// const inter = Inter({ subsets: ['latin'] })
 
 // Random number from 1 to 122
 const randomImg = (): number => Math.floor(Math.random() * 122) + 1
 
+// Generate unique id
+const generateId = () => Math.random().toString(36).substr(2, 9)
+
+type imageItem = { id: string, url: string }
+
 const Home: NextPage = () =>  {
+  const [ images, setImages ] = useState<Array<imageItem>>([
+    { id: generateId(), url: `https://randomfox.ca/images/${randomImg()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${randomImg()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${randomImg()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${randomImg()}.jpg` },
+  ])
   return (
     <main>
       <h2>Hola, mundillo ;)</h2>
-      <RandomFox image={ `https://randomfox.ca/images/${randomImg()}.jpg` }/> 
+      { images.map((image) => (
+        <div key={ image.id } className=' w-screen flex flex-col items-center p-4'>
+          <RandomFox image={ image.url }/> 
+        </div>
+      ))}
     </main>
   )
 }
