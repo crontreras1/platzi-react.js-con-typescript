cccb5a5289ccd20f8a87b4731227ed1072549603
crontreras1 <crontreras1@gmail.com>
2023-12-28 11:02:51 -0500

    class 9. tipes to references and observers

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,7 +1,11 @@
+import { useRef } from "react"
+
 type Props = {
     image: string;
 }
 
 export const RandomFox = ({ image }: Props): JSX.Element => {
-    return <img src={ image } className="w-96 h-auto"/>  
+    const node = useRef<HTMLImageElement>(null)
+    
+    return <img ref={ node } src={ image } className="w-96 h-auto"/>  
 }
